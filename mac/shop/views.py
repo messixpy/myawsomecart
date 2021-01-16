3dd4f919ef571b531998e8a2a88aa8cd1b3df22c
Code Messi <sameerjee9500@gmail.com>
2021-01-16 17:40:48 +0500

    Initial commit

---
+++ mac/shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+def index(request):
+    products = Product.objects.all()
+    print(products)
+    n=len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+
+    params= {'no_of_slides':nSlides , 'range': range(1,nSlides), 'product':products}
+    allProds=[[products, range(1, nSlides), nSlides],[products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request,'shop/index.html', params)
+
+def about(request):
+    return render(request,'shop/about.html')
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
